30ba496bbf7ecd08ffcb30b8aaec08b96260e01a
Charissa Ramirez <chawissa@gmail.com>
2021-04-05 21:18:09 -0400

    Grids

---
+++ index.js
@@ -0,0 +1,9 @@
+for (let i = 0; i < 16; i++) {
+  let row = document.createElement("div");
+  for (let j = 0; j < 16; j++) {
+    let cell = document.createElement("div");
+    cell.classList.add("cell");
+    row.appendChild(cell);
+  }
+  document.getElementById("container").appendChild(row);
+}
