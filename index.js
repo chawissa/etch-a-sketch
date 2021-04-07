56c6fefbc267939e96a6c18c9673b288202aea1d
Charissa Ramirez <chawissa@gmail.com>
2021-04-06 20:31:43 -0400

    16x16 grid

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-for (let i = 0; i < 16; i++) {
-  let row = document.createElement("div");
-  for (let j = 0; j < 16; j++) {
-    let cell = document.createElement("div");
-    cell.classList.add("cell");
-    row.appendChild(cell);
+// for (let i = 0; i < 16; i++) {
+//   let row = document.createElement("div");
+//   for (let j = 0; j < 16; j++) {
+//     let cell = document.createElement("div");
+//     cell.classList.add("cell");
+//     row.appendChild(cell);
+//   }
+//   document.getElementById("container").appendChild(row);
+// }
+
+const grid = document.querySelector(".container");
+
+const createGrid = function () {
+  for (let i = 0; i < 256; i++) {
+    const div = document.createElement("div");
+    div.classList.add("square");
+    grid.appendChild(div);
   }
-  document.getElementById("container").appendChild(row);
-}
+};
+
+createGrid();
